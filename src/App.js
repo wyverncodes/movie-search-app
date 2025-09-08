cc7a8d441e75833fec0d925f710743b2a207aa3c
Wyvern <138558065+wyverncodes@users.noreply.github.com>
2025-09-08 18:20:31 +0530

    commiting this to main branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import SearchMovies from './searchMovie'
+export default function App()
+{
+  return(
+    <div className="container">
+        <h1 className="title">Movie Search App</h1>
+        <SearchMovies/>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/searchMovie.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import MovieCard from "./movieCard";
+export default function SearchMovie() {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async (e) => {
+    e.preventDefault();
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=2c4a2135688fc6697191c3e98b155f50&language=en-US&query=${query}&page=1&include_adult=false`;
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+      console.log(data.results);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={searchMovies}>
+        <label className="label" htmlFor="query">
+          Movie Name
+        </label>
+        <input
+          className="input"
+          type="text"
+          name="query"
+          placeholder="i.e.jurassic park"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        ></input>
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="card-list">
+        {movies.filter(movie=>movie.poster_path).map((movie) => (
+         <MovieCard  movie={movie} key={movie.id}/>
+        ))}
+      </div>
+    </>
+  );
+}
