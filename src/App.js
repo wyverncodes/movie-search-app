0ba6b59601c2870aca73d671d68d022f203d0347
Wyvern <138558065+wyverncodes@users.noreply.github.com>
2025-09-08 18:22:59 +0530

    commiting this to second branch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App()
   return(
     <div className="container">
         <h1 className="title">Movie Search App</h1>
+        <h2 className='title'>lol u reallly think u will get the job cause u made this xd </h2>
         <SearchMovies/>
     </div>
   )
